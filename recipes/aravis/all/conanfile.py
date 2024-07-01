2bdc6effdd4e259ea207b1aebf7d1a8a7e3383b6
Václav Šmilauer <eu@doxos.eu>
2024-07-01 08:37:23 +0200

    relax glib and libxml2 deps (to avoid conflicts with e.g. opencv)

--- recipes/aravis/all/conanfile.py
+++ recipes/aravis/all/conanfile.py
@@ -72,8 +72,8 @@ class AravisConan(ConanFile):
 
     def requirements(self):
         # glib-object.h and gio/gio.h are used in several public headers
-        self.requires("glib/2.78.1", transitive_headers=True)
-        self.requires("libxml2/2.12.3")
+        self.requires("glib/[>=2.78.1 <3]", transitive_headers=True)
+        self.requires("libxml2/[>=2.12.3 <3]")
         self.requires("zlib/[>=1.2.11 <2]")
 
         if self.options.usb:
